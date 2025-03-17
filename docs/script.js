79a915c7878602e9f8ee01d951aa3f5fd295904f
phorton1 <available@upon.request>
2025-03-17 09:29:41 -0500

    github pages test

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,9 @@ function toggle(element) {
     const nestedList = element.nextElementSibling;
     if (nestedList.style.display === "block") {
         nestedList.style.display = "none";
+        element.classList.remove('expanded'); // Remove expanded class
     } else {
         nestedList.style.display = "block";
+        element.classList.add('expanded'); // Add expanded class
     }
 }
