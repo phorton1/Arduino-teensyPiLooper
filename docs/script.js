397f98880440f2125215bd4a4e6f59bd5a7e00a0
phorton1 <available@upon.request>
2025-03-17 09:59:01 -0500

    github pages test

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,16 @@
 // script.js
 
-function toggle(element) {
+function toggle(event, element) {
+    // Prevent the default link action if the element has children
     const nestedList = element.nextElementSibling;
-    if (nestedList.style.display === "block") {
-        nestedList.style.display = "none";
-        element.classList.remove('expanded'); // Remove expanded class
-    } else {
-        nestedList.style.display = "block";
-        element.classList.add('expanded'); // Add expanded class
+    if (nestedList) {
+        event.preventDefault(); // Prevent navigation if it has children
+        if (nestedList.style.display === "block") {
+            nestedList.style.display = "none";
+            element.classList.remove('expanded'); // Remove expanded class
+        } else {
+            nestedList.style.display = "block";
+            element.classList.add('expanded'); // Add expanded class
+        }
     }
 }
